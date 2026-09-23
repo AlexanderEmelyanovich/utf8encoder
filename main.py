97783dc8e616538a016484f74c940cbde3e15fe6
AlexanderEmelyanovich <andreew.sacha2010@yandex.com>
2026-09-23 13:10:08 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
-from kivy.core.window import Window  # <-- это было пропущено
+from kivy.core.window import Window
 
 
 class EncoderApp(App):
